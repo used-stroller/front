691dfc7931ed89c70dbe959de8f681df084af08a
clowncdi <clowncdi85@gmail.com>
2024-03-07 23:37:53 +0900

    1. deploy 설정 변경

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# 어드민 계정 접속
-sudo su;
-echo 'su access';
-
-# 프로젝트 실행
 cd /home/aoiwejpgoajwpoejfpoa/fe/front
-npm run deploy;
+sudo npm run deploy;
 echo 'jungmocha project start';
 exit;
