982e93881a2895906b8f645fd55070edb3020a18
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-20 21:45:20 +0900

    feat : Google Analytics 설치

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,14 @@ import Ad from "@/components/Ad";
 import type { ReactElement } from "react";
 import Logo from "@/components/Logo";
 import styles from "@/styles/about.module.css";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-B2KK9DNYZ1");
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname, 
+  title: "Custom Title" 
+});
 export default function About(): ReactElement {
   return (
     <div className={styles.contents}>
