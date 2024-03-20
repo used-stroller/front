ecb85b68669b811c0ceff28ac397777d3ce1e6ca
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-20 22:07:02 +0900

    fix : CRLF -> LF

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,10 @@ import styles from "@/styles/about.module.css";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-B2KK9DNYZ1");
-ReactGA.send({ 
-  hitType: "pageview", 
-  page: window.location.pathname, 
-  title: "Custom Title" 
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Custom Title",
 });
 export default function About(): ReactElement {
   return (
