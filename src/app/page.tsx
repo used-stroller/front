d580c7b484583c3429f3e6686ed4baf829fd7a4e
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-21 22:00:34 +0900

    test : ga4 test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import SearchBarFilter from "@/components/SearchBarFilter";
 import ProductList from "@/components/ProductList";
 import type { ReactElement } from "react";
-import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-B2KK9DNYZ1");
-ReactGA.send("pageview");
 export default function Home(): ReactElement {
   return (
     <>
