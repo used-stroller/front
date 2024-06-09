c39117d64c98bdb680194dd82a728d59b9003472
hoonyhoney <hoonyhoeny@gmail.com>
2024-06-09 22:12:51 +0900

    fix : ad 문구 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer(): ReactElement {
       <i className={styles.separator} />
       <div>
         <p className={styles.address}>
-          @제휴 및 기타 문의@
+          제휴 및 기타 문의
           <a href="mailto:hoonyhoeny@gmail.com">hoonyhoeny@gmail.com</a>
         </p>
         <p>Copyright © {year} 중모차</p>
