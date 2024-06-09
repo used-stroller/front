bf956ff80c3a59f88b5287a56bd36068e1d67344
hoonyhoney <hoonyhoeny@gmail.com>
2024-06-09 15:08:22 +0900

    fix : eslint 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
   title: "똑똑한 엄빠의 중모차",
   description:
     "중고유모차 구매팁! 중고플랫폼의 유모차매물만 모아서 보여드립니다. 중고유모차 살때 , 똑똑한 엄빠의 선택 “중모차”에서 검색하세요! ",
-  keywords: "중모차, 중고 유모차, 부가부, 스토케, 줄즈, 절충형, 디럭스, 중고유모차 구매팁, 중고유모차 살때, 중고유모차 팔때",
+  keywords:
+    "중모차, 중고 유모차, 부가부, 스토케, 줄즈, 절충형, 디럭스, 중고유모차 구매팁, 중고유모차 살때, 중고유모차 팔때",
   applicationName: "중모차",
   authors: [{ name: "Team Three", url: "hoonyhoeny@gmail.com" }],
   robots: "index, follow",
