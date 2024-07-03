0f238e90938a8140d70137b0d2c2c183a7e658b9
hoonyhoney <hoonyhoeny@gmail.com>
2024-07-03 23:13:59 +0900

    Revert "위치권한 거부시 재승인 테스트"
    This reverts commit a96dcd1292fcf831b1ac4dfdb22a1b9d0ac8e313.

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -6,14 +6,11 @@ interface Coordinates {
   longitude: number | null;
 }
 
-const GEOLOCATION_DENIED_KEY = "geolocationDenied";
-
 const getGeolocation = async (): Promise<Coordinates> => {
   return await new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
-        localStorage.removeItem(GEOLOCATION_DENIED_KEY);  // 권한이 허용된 경우 상태 제거
         resolve({ latitude, longitude });
       },
       (error) => {
@@ -22,7 +19,6 @@ const getGeolocation = async (): Promise<Coordinates> => {
             alert(
               "위치 정보 제공을 거부했습니다. 현재 위치 근처의 당근마켓 제품을 확인하려면 브라우저 설정에서 위치 권한을 허용해 주세요.",
             );
-            localStorage.setItem(GEOLOCATION_DENIED_KEY, "true"); // 권한 거부 상태 저장
             reject(new Error("위치 정보 제공을 거부했습니다."));
             break;
           case error.POSITION_UNAVAILABLE:
@@ -52,33 +48,15 @@ const useGeolocation = (): {
 
   useEffect(() => {
     if (isMobile()) {
-      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-      const checkPermission = async () => {
-        try {
-          const permissionState = await navigator.permissions.query({
-            name: "geolocation",
-          });
-
-          if (permissionState.state === 'denied') {
-            localStorage.setItem(GEOLOCATION_DENIED_KEY, 'true');
-          } else {
-            localStorage.removeItem(GEOLOCATION_DENIED_KEY);
-            getGeolocation()
-              .then((coords) => {
-                setLocation(coords);
-                setLoading(false);
-              })
-              .catch((err) => {
-                setError(err.message as string);
-                setLoading(false);
-              });
-          }
-        } catch (err) {
-          console.error('Permission query error: ', err);
-        }
-      };
-
-      void checkPermission();
+      getGeolocation()
+        .then((coords) => {
+          setLocation(coords);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message as string);
+          setLoading(false);
+        });
     }
   }, []);
 
