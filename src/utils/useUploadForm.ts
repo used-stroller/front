2d3094d1cadec8bcba0d057fb813db3198b00123
hoonyhoney <hoonyhoeny@gmail.com>
2024-11-03 18:40:03 +0900

    feat: zustand 전역관리

---
+++ src/utils/useUploadForm.ts
@@ -0,0 +1,41 @@
+import { type image } from "@/types";
+import { create } from "zustand";
+
+interface FormState {
+  selectedStatus: string;
+  selectedPeriod: string;
+  selectedOptions: string[];
+  text: string;
+  images: image[];
+
+  setSelectedStatus: (value: string) => void;
+  setSelectedPeriod: (value: string) => void;
+  setSelectedOptions: (updater: (prev: string[]) => string[]) => void; // 함수형 업데이트를 지원하도록 타입 정의
+  setText: (value: string) => void;
+  setImages: (value: image[]) => void;
+}
+
+export const useUploadForm = create<FormState>((set) => ({
+  selectedStatus: "새상품",
+  selectedPeriod: "1",
+  selectedOptions: [],
+  text: "",
+  images: [],
+  setSelectedStatus: (value) => {
+    set({ selectedStatus: value });
+  },
+  setSelectedPeriod: (value) => {
+    set({ selectedPeriod: value });
+  },
+  setSelectedOptions: (updater) => {
+    set((state) => ({
+      selectedOptions: updater(state.selectedOptions),
+    }));
+  },
+  setText: (value) => {
+    set({ text: value });
+  },
+  setImages: (value) => {
+    set({ images: value });
+  },
+}));
