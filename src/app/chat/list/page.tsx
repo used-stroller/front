26bb6e5f997df58d8123e1b96447b24000a3c220
dhlee <hoonyhoeny@gmail.com>
2025-06-10 12:15:16 +0900

    feat: 채팅방리스트, 바텀바 jwt확인로직 추가

---
+++ src/app/chat/list/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import styles from "@/styles/chatList.module.css";
+import apiClient from "@/utils/apiClient";
+import { formatDistanceToNow } from "date-fns";
+import { ko } from "date-fns/locale";
+
+interface ChatRoom {
+  roomId: string;
+  productImageUrl: string;
+  opponentName: string;
+  lastMessage: string;
+  lastMessageTime: string;
+  unreadCount: number;
+}
+
+export default function ChatList() {
+  const router = useRouter();
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  const handleClick = (id: string): void => {
+    console.log("이동할 방 ID:", id);
+    router.push(`/chat/${id}`);
+  };
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const { data } = await apiClient.get("/api/chat/list");
+        setChatRooms(data);
+      } catch (error) {
+        console.error("⚠️ 메시지 불러오기 실패:", error);
+      }
+    };
+
+    void fetchData();
+  }, []);
+
+  return (
+    <div className={styles.chatList}>
+      {chatRooms.map((room) => (
+        <div
+          key={room.roomId}
+          role="button"
+          tabIndex={0}
+          className={styles.chatItem}
+          onClick={() => {
+            handleClick(room.roomId);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              handleClick(room.roomId);
+            }
+          }}
+        >
+          <img
+            src={room.productImageUrl}
+            alt="상품 이미지"
+            className={styles.image}
+          />
+          <div className={styles.chatContent}>
+            <div className={styles.topRow}>
+              <span className={styles.nickname}>{room.opponentName}</span>
+              <span className={styles.location}>
+                {formatDistanceToNow(new Date(room.lastMessageTime), {
+                  addSuffix: true,
+                  locale: ko,
+                })}
+              </span>
+            </div>
+            <div className={styles.bottomRow}>
+              <span className={styles.lastMessage}>{room.lastMessage}</span>
+              {room.unreadCount > 0 && (
+                <span className={styles.unread}>{room.unreadCount}</span>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -4,18 +4,24 @@ import styles from "@/styles/bottomNav.module.css";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { HiHome } from "react-icons/hi";
-import { FaRegClipboard, FaRegUser } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-import { MdLocationOn } from "react-icons/md";
+import { IoMdAddCircle } from "react-icons/io";
+import { getCookie } from "@/utils/cookie";
+import { FaRegUser } from "react-icons/fa";
 
 const navItems = [
   { href: "/", label: "홈", icon: <HiHome size={24} /> },
-  { href: "/life", label: "동네생활", icon: <FaRegClipboard size={22} /> },
-  { href: "/map", label: "동네지도", icon: <MdLocationOn size={24} /> },
   {
-    href: "/chat",
+    href: "/upload-product", // 무조건 여기에 이동할 경로 명시
+    label: "상품등록",
+    icon: <IoMdAddCircle size={26} />,
+    protected: true, // JWT 검증 필요
+  },
+  {
+    href: "/chat/list",
     label: "채팅",
     icon: <IoChatbubbleEllipsesOutline size={24} />,
+    protected: true, // JWT 검증 필요
   },
   { href: "/mypage", label: "나의 당근", icon: <FaRegUser size={22} /> },
 ];
@@ -27,8 +33,24 @@ export default function BottomNav() {
     <nav className={styles.nav}>
       {navItems.map((item) => {
         const isActive = pathname === item.href;
+        const handleClick = (e: React.MouseEvent) => {
+          if (item.protected) {
+            e.preventDefault();
+            const jwtToken = getCookie("jwt");
+            if (jwtToken) {
+              window.location.href = item.href;
+            } else {
+              window.location.href = "/signin";
+            }
+          }
+        };
         return (
-          <Link key={item.href} href={item.href} className={styles.link}>
+          <Link
+            key={item.href}
+            href={item.href}
+            className={styles.link}
+            onClick={handleClick}
+          >
             <div className={styles.item}>
               <div
                 className={`${styles.icon} ${isActive ? styles.active : ""}`}
