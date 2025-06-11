a26e4375997d64f287d9df87f73e8b7c50bc25c0
hoonyhoney <hoonyhoeny@gmail.com>
2025-06-12 00:53:00 +0900

    ㅎ

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -24,7 +24,8 @@ export default function Chat() {
 
   // ✅ 1. 소켓 연결 - 최초 1회
   useEffect(() => {
-    socketRef.current = io("http://182.228.21.172:9092", {
+    socketRef.current = io("https://jungmocha.co.kr", {
+      path: "/socket.io",
       transports: ["polling", "websocket"],
       reconnection: false,
     });
