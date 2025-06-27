81bcfe1dfd1d137397d0c72acf73a23449c3c3bd
hoonyhoney <hoonyhoeny@gmail.com>
2025-06-27 16:30:42 +0900

    fix: 이미지 슬라이더 수정

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -17,12 +17,12 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, settings }) => {
     speed: 500, // 슬라이드 전환 속도
     slidesToShow: 1, // 한 번에 보여줄 슬라이드 개수
     slidesToScroll: 1, // 한 번에 넘길 슬라이드 개수
-    adaptiveHeight: true, // 슬라이드 높이를 자동 조정
+    adaptiveHeight: false, // 슬라이드 높이를 자동 조정
     ...settings, // 전달된 설정으로 기본 설정 덮어쓰기
   };
 
   return (
-    <div style={{ width: "100%", margin: "0 auto", overflow: "hidden" }}>
+    <div style={{ width: "100%", margin: "0 auto", paddingBottom: "30px" }}>
       <Slider {...defaultSettings}>
         {images.map((src, index) => (
           <div key={index}>
