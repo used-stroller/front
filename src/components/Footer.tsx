395135a4728643905521572f986893910f6a94c3
dhlee <hoonyhoeny@gmail.com>
2025-08-22 17:59:52 +0900

    fix: 푸터 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,13 +23,27 @@ export default function Footer(): ReactElement {
       <i className={styles.separator} />
       <div>
         <p className={styles.address}>{/* 제휴 및 기타 문의 */}</p>
-        <p>상호명: 중모차</p>
-        <p>대표자: 이동훈</p>
-        <p>사업자등록번호: 863-21-01666</p>
-        <p>통신판매업신고번호: 2025-경기김포-6176 호</p>
-        <p>주소: 경기도 김포시 김포한강 11로 276</p>
-        <p>전화: 010-8113-6030</p>
-        <p>개인정보보호책임자:admin@jungmocha.co.kr</p>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "12px",
+          }}
+        >
+          <span>상호명: 중모차</span>
+          <span>대표자: 이동훈</span>
+          <span>사업자등록번호: 863-21-01666</span>
+        </div>
+        <div>
+          <span>통신판매업신고번호: 2025-경기김포-6176 호</span>
+        </div>
+        <div>
+          <span>주소: 경기도 김포시 김포한강 11로 276</span>
+        </div>
+        <div>
+          {/* <span>전화: 010-8113-6030</span>
+          <span>개인정보보호책임자: admin@jungmocha.co.kr</span> */}
+        </div>
         <p>Copyright © {year} 중모차</p>
       </div>
     </div>
